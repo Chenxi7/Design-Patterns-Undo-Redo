deff66650dd739a27b45740eacaecf9f598aa17e
Cathy <cathy@CathydeMacBook-Pro.local>
2016-12-14 15:55:02 +0800

    Initial Commit

---
+++ undo-redo-four/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  undo-redo-four
+//
+//  Created by Cathy on 2016/12/14.
+//  Copyright © 2016年 Cathy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
